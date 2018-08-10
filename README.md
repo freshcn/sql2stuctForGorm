1d8194308c7a4d17cfef5a50249ee2e52137f496
chenzhouyu <chenzhouyu@qq.com>
2018-08-10 12:08:26 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,4 @@
+本项目是基于[sql2sturct](https://github.com/mikemintang/sql2struct)做的修改，感谢原作者做出的贡献。
+
+ - 修改为直接使用网页来使用，另外添加对comment的支持能力。
+ - 添加将sql中的comment作为注释信息
---
+++ js/sql2struct.js
@@ -0,0 +1,220 @@
+Vue.use(VueLocalStorage);
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            cache: null,
+            sqlContent: '',
+            structContent: '',
+            activeIndex: '1',
+            typeMap: getTypeMap(),
+            typeMapStr: '',
+            useGorm: true,
+            useJson: true,
+            useForm: true,
+            dialogFormVisible: false
+        }
+    },
+    created() {
+        
+        var that = this,
+            res = this.$localStorage.get("sql2stuctForGorm");
+        // 获取缓存数据
+        if (!res) { // 不存在缓存数据
+            // 初始配置数据
+            var data = {
+                useGorm: that.useGorm,
+                useJson: that.useJson,
+                useForm: that.useForm,
+                typeMap: that.typeMap
+            }
+            that.setCache(data)
+            for (var k in that.typeMap) {
+                that.typeMapStr += k + ': ' + that.typeMap[k] + '\n'
+            }
+            return
+        }
+        var obj = JSON.parse(res)
+        if (obj.useGorm != undefined) {
+            that.useGorm = obj.useGorm
+        }
+        if (obj.useJson != undefined) {
+            that.useJson = obj.useJson
+        }
+        if (obj.useForm != undefined) {
+            that.useForm = obj.useForm
+        }
+        if (obj.typeMap != undefined) {
+            that.typeMap = obj.typeMap
+            for (var k in obj.typeMap) {
+                that.typeMapStr += k + ': ' + obj.typeMap[k] + '\n'
+            }
+        }
+    },
+    watch: {
+        sqlContent(val) {
+            if (!val) {
+                this.structContent = ''
+                return
+            }
+            var res = val.split('\n')
+            if (!res) {
+                this.structContent = 'invalid sql'
+                return
+            }
+            var tableComment = res[res.length-1].match(/comment=\'(.*)\'/i)
+            var types = this.typeMap
+            var structResult = 'type '
+            // console.log(res)
+            for (var i = 0, len = res.length; i < len; i++) {
+                // console.log('res:', i, '  ---', res[i])
+                var field = res[i].match(/\`(.+)\`\s+((tinyint|smallint|int|mediumint|bigint|float|double|decimal|varchar|char|text|mediumtext|longtext|datetime|time|date|enum|set|blob)?[\([\d]+\)]?)?([\w\s\'\.]+(comment\s\'(.*)\'))?/i)
+
+                    // console.log(field)
+                if (i == 0) { // 第一个字段为数据表名称
+                    if (field && field[1] != undefined && field[2] == undefined) {
+                        var tbName = titleCase(field[1])
+                        if ( tableComment.length > 0 ) {
+                            structResult = '// '+ tbName + ' ' + tableComment[1] + '\n' + structResult;
+                        }
+                        structResult += tbName + ' struct {'
+                        continue
+                    } else {
+                        return
+                    }
+                } else { // 数据表字段
+                    if (field && field[1] != undefined && field[2] != undefined && field[3] != undefined) {
+                        if (types[field[3]] != undefined) {
+                            var fieldName = titleCase(field[1])
+                            var fieldType = types[field[3]]
+                            var fieldJsonName = field[1].toLowerCase()
+                            if (fieldName.toLowerCase() == 'id') {
+                                fieldName = 'ID'
+                            }
+                            if (field[6] != undefined) {
+                                structResult += '\n\t// ' + fieldName + ' ' + field[6]
+                            }
+                            structResult += '\n\t' + fieldName + ' ' + fieldType + ' '
+                            structArr = []
+                            if (this.useGorm) {
+                                var gorm = ["column:"+fieldJsonName, "type:"+field[2]]
+
+
+                                structArr.push('gorm:"'+gorm.join(';')+'"')
+                            }
+                            if (this.useJson) {
+                                structArr.push('json:"' + fieldJsonName + '"')
+                            }
+                            if (this.useForm) {
+                                structArr.push('form:"' + fieldJsonName + '"')
+                            }
+                            if (structArr.length > 0) {
+                                structResult += '`' + structArr.join(' ') + '`'
+                            }
+                        } else {
+                            continue
+                        }
+                    } else {
+                        continue
+                    }
+                }
+            }
+            structResult += '\n}'
+            this.structContent = structResult
+        },
+        typeMapStr(val) {
+            var typeArr = val.split('\n')
+            var typeMap = {}
+            for (var i = 0, len = typeArr.length; i < len; i++) {
+                var itemArr = typeArr[i].split(/\:\s+/)
+                if (itemArr[0] != undefined && itemArr[1] != undefined) {
+                    typeMap[itemArr[0]] = itemArr[1]
+                }
+            }
+            this.typeMap = typeMap
+            var data = {
+                useGorm: this.useGorm,
+                useJson: this.useJson,
+                useForm: this.useForm,
+                typeMap: this.typeMap
+            }
+            this.setCache(data)
+        },
+        useGorm(val) {
+            this.useGorm = val
+            var data = {
+                useGorm: this.useGorm,
+                useJson: this.useJson,
+                useForm: this.useForm,
+                typeMap: this.typeMap
+            }
+            this.setCache(data)
+        },
+        useJson(val) {
+            this.useJson = val
+            var data = {
+                useGorm: this.useGorm,
+                useJson: this.useJson,
+                useForm: this.useForm,
+                typeMap: this.typeMap
+            }
+            this.setCache(data)
+        },
+        useForm(val) {
+            this.useForm = val
+            var data = {
+                useGorm: this.useGorm,
+                useJson: this.useJson,
+                useForm: this.useForm,
+                typeMap: this.typeMap
+            }
+            this.setCache(data)
+        }
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+
+        },
+        setCache(data) {
+            this.$localStorage.set('sql2stuctForGorm', JSON.stringify(data))
+        }
+    }
+})
+
+// 首字母大写
+function titleCase(str) {
+
+    var array = str.toLowerCase().split("_");
+    for (var i = 0; i < array.length; i++) {
+        array[i] = array[i][0].toUpperCase() + array[i].substring(1, array[i].length);
+    }
+    var string = array.join("");
+
+    return string;
+}
+
+// 类型映射
+function getTypeMap() {
+    return {
+        'tinyint': 'int',
+        'smallint': 'int',
+        'int': 'int',
+        'mediumint': 'int64',
+        'bigint': 'int64',
+        'float': 'float64',
+        'double': 'float164',
+        'decimal': 'float64',
+        'char': 'string',
+        'varchar': 'string',
+        'text': 'string',
+        'mediumtext': 'string',
+        'longtext': 'string',
+        'time': 'time.Time',
+        'date': 'time.Time',
+        'datetime': 'time.Time',
+        'timestramp': 'int64',
+        'enum': 'string',
+        'set': 'string',
+        'blob': 'string'
+    }
+}
