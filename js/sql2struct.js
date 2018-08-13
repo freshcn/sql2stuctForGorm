78e0fc2c46253ea77c058aec8da21addee8f7822
chenzhouyu <chenzhouyu@qq.com>
2018-08-13 17:08:20 +0800

    添加索引、not null、auto_increment支持

--- js/sql2struct.js
+++ js/sql2struct.js
@@ -65,13 +65,46 @@ new Vue({
             var tableComment = res[res.length-1].match(/comment=\'(.*)\'/i),
             types = this.typeMap,
             structResult = 'type ',
-            pk = val.match(/PRIMARY KEY\s\(`([A-Za-z_]+)`\)/);
-            // console.log(res)
+            pk = [],
+            unique = {},
+            indexs = {},
+            keys = val.match(/((PRIMARY|UNIQUE)\s)?KEY\s([\`|\w|_]+\s)?\((\w|_|\`|,)+\)/g);
+
+            // 开始处理索引相关
+            if (keys.length > 0) {
+                var tmpFields = [];
+                keys.forEach(function(k){
+                    tmpFields = k.match(/\`(\w|_)+\`/g);
+                    for (var i = 0; i < tmpFields.length; i++) {
+                        tmpFields[i] = tmpFields[i].replace(/\`/g, '');
+                    }
+                    if (k.indexOf("PRIMARY KEY ") == 0 ){
+                        pk = tmpFields
+                    }
+
+                    if (k.indexOf("UNIQUE KEY") == 0) {
+                        for (var ii = 1; ii < tmpFields.length; ii ++) {
+                            if (unique[tmpFields[ii]] == undefined) {
+                                unique[tmpFields[ii]] = [];
+                            } 
+                            unique[tmpFields[ii]].push(tmpFields[0])
+                        }
+                       
+                    }
+
+                    if (k.indexOf("KEY") == 0) {
+                        for (var ii = 1; ii < tmpFields.length; ii ++) {
+                            if (indexs[tmpFields[ii]] == undefined) {
+                                indexs[tmpFields[ii]] = [];
+                            } 
+                            indexs[tmpFields[ii]].push(tmpFields[0])
+                        }
+                    }
+                })
+            }
             for (var i = 0, len = res.length; i < len; i++) {
-                // console.log('res:', i, '  ---', res[i])
                 var field = res[i].match(/\`(.+)\`\s+((tinyint|smallint|int|mediumint|bigint|float|double|decimal|varchar|char|text|mediumtext|longtext|datetime|time|date|enum|set|blob)?[\([\d]+\)]?)?([\w\s\'\.]+(comment\s\'(.*)\'))?/i)
 
-                    // console.log(field)
                 if (i == 0) { // 第一个字段为数据表名称
                     if (field && field[1] != undefined && field[2] == undefined) {
                         var tbName = titleCase(field[1])
@@ -100,9 +133,26 @@ new Vue({
                             if (this.useGorm) {
                                 var gorm = ["column:"+fieldJsonName, "type:"+field[2]]
 
-                                if (pk[1] != undefined && field[1] == pk[1]) {
-                                    gorm.push("PRIMARY_KEY")
+                                if (pk.indexOf(field[1]) >= 0) {
+                                    gorm.push("primary_key")
                                 }
+
+                                if (unique[field[1]] != undefined) {
+                                    gorm.push("unique_index:"+unique[field[1]].join(','))
+                                }
+
+                                if (indexs[field[1]] != undefined) {
+                                    gorm.push("index:"+indexs[field[1]].join(','))
+                                }
+
+                                if (res[i].indexOf("NOT NULL") > 0) {
+                                    gorm.push("not null")
+                                }
+
+                                if (res[i].indexOf("AUTO_INCREMENT") > 0) {
+                                    gorm.push("auto_increment")
+                                }
+
                                 structArr.push('gorm:"'+gorm.join(';')+'"')
                             }
                             if (this.useJson) {
